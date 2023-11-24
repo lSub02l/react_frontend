6863b813cb1c6092d7cd9d70c3b2dd9ae424ae0f
Adrian Flak <bazylipiotr@gmail.com>
2023-11-24 15:20:56 +0100

    first commit

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,77 @@
+// src/components/LoginPage.js
+import React, { useState } from 'react';
+import WithSubnavigation from '../components/Navigation'
+import httpClient from '../utils/httpClient';
+import { useNavigate} from 'react-router-dom';
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Heading,
+  VStack,
+  useToast,
+} from '@chakra-ui/react';
+
+
+const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [psswd, setpsswd] = useState('');
+    const toast = useToast()
+    const navigation = useNavigate()
+
+    const handleLogin = async () => {
+        const {data} = await httpClient.post("/login", {email,psswd} )
+        console.log(data)
+
+        toast({
+            title: data["message"],
+            status: data["ok"] ? "success" : "error",
+            duration: 4000,
+            isClosable: true,
+        })  
+        if (data['ok']) {
+            navigation("/")
+        }
+    };
+
+    return (
+        <div>
+        <WithSubnavigation/>
+        <VStack marginTop={20} spacing={8} align="center">
+        <Box p={8} borderWidth={1} borderRadius={8} boxShadow="lg" width="400px">
+            <Heading mb={4}>Login</Heading>
+            <FormControl>
+            <FormLabel>E-mail</FormLabel>
+            <Input
+                type="text"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            </FormControl>
+            <FormControl mt={4}>
+            <FormLabel>psswd</FormLabel>
+            <Input
+                type="password"
+                placeholder="Enter your password"
+                value={psswd}
+                onChange={(e) => setpsswd(e.target.value)}
+            />
+            </FormControl>
+            <Button
+            colorScheme="teal"
+            mt={4}
+            onClick={handleLogin}
+            isFullWidth
+            >
+            Login
+            </Button>
+        </Box>
+        </VStack>
+        </div>
+  );
+};
+
+export default LoginPage;
